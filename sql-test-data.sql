860aa06f97612d67f874e3d559ce22402cd52070
gabrielradu <radu.gabi93@gmaiil.com>
2022-10-12 17:23:17 +0300

    clean up

--- sql-test-data.sql
+++ sql-test-data.sql
@@ -1,3 +1,3 @@
-insert into db.Client (id, name, code, is_active, created_datetime, updated_datetime) values (1, 'bayut', 'BAYUT', 1, null, null);
-insert into db.Client (id, name, code, is_active, created_datetime, updated_datetime) values (3, 'sportisimo', 'SPORTISIMO', 1, null, null);
-insert into db.Client (id, name, code, is_active, created_datetime, updated_datetime) values (4, 'buzzfeed', 'BUZZFEED', 1, null, null);
+insert into db.Target (id, name, code, is_active, created_datetime, updated_datetime) values (1, 'bayut', 'BAYUT', 1, null, null);
+insert into db.Target (id, name, code, is_active, created_datetime, updated_datetime) values (3, 'sportisimo', 'SPORTISIMO', 1, null, null);
+insert into db.Target (id, name, code, is_active, created_datetime, updated_datetime) values (4, 'buzzfeed', 'BUZZFEED', 1, null, null);
